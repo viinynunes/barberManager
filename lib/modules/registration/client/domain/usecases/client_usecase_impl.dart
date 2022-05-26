04eeff05fc6bb63df4d93ddeae9160fe7fffb381
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-05-26 16:14:05 -0300

    Validator fields in separated class

--- lib/modules/registration/client/domain/usecases/client_usecase_impl.dart
+++ lib/modules/registration/client/domain/usecases/client_usecase_impl.dart
@@ -2,8 +2,8 @@ import 'package:barbar_manager/modules/registration/client/domain/entities/clien
 import 'package:barbar_manager/modules/registration/client/domain/errors/client_errors.dart';
 import 'package:barbar_manager/modules/registration/client/domain/repositories/client_repository.dart';
 import 'package:barbar_manager/modules/registration/client/domain/usecases/client_usecase.dart';
+import 'package:barbar_manager/modules/registration/client/domain/utils/validate_client_fields.dart';
 import 'package:dartz/dartz.dart';
-import 'package:string_validator/string_validator.dart';
 
 class ClientUsecaseImpl implements ClientUsecase {
   final ClientRepository _repository;
@@ -11,17 +11,11 @@ class ClientUsecaseImpl implements ClientUsecase {
   ClientUsecaseImpl(this._repository);
 
   @override
-  Future<Either<ClientErrors, Client>> call(Client client) async {
-    if (!isEmail(client.email)) {
-      return Left(ClientValidatorError('Invalid Email'));
-    }
+  Future<Either<ClientErrors, Client>> create(Client client) async {
+    final validator = ValidateClientFields.validate(client);
 
-    if (client.name.isEmpty) {
-      return Left(ClientValidatorError('Invalid Name'));
-    }
-
-    if (client.phoneNumber.isEmpty || client.phoneNumber.length != 11) {
-      return Left(ClientValidatorError('Invalid Phone Number'));
+    if (validator.isLeft()) {
+      return validator;
     }
 
     return await _repository.create(client);
---
+++ lib/modules/registration/client/domain/utils/validate_client_fields.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+import 'package:string_validator/string_validator.dart';
+
+import '../entities/client.dart';
+import '../errors/client_errors.dart';
+
+class ValidateClientFields {
+
+  static Either<ClientErrors, Client> validate(Client client) {
+    if (!isEmail(client.email)) {
+      return Left(ClientValidatorError('Invalid Email'));
+    }
+
+    if (client.name.isEmpty) {
+      return Left(ClientValidatorError('Invalid Name'));
+    }
+
+    if (client.phoneNumber.isEmpty || client.phoneNumber.length != 11) {
+      return Left(ClientValidatorError('Invalid Phone Number'));
+    }
+
+    return Right(client);
+  }
+}
