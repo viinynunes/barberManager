34fbfcac91d6e82fcfed8904c99e603ab8810308
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-05-27 14:29:38 -0300

    Tests group to delete function

--- test/modules/registration/client/usecases/client_usecase_test.dart
+++ test/modules/registration/client/usecases/client_usecase_test.dart
@@ -19,46 +19,57 @@ main() {
     when(repository.createOrUpdate(any))
         .thenAnswer((realInvocation) async => Right(client));
 
-    when(repository.delete(any)).thenAnswer((realInvocation) async => const Right(true));
+    when(repository.delete(any))
+        .thenAnswer((realInvocation) async => const Right(true));
   });
 
   group('tests to create or update a client', () {
     test('should return a ClientError when name is empty', () async {
-      final result = await useCase.createOrUpdate(Client('', '19981436342', 'viny@gmail.com', true));
+      final result = await useCase
+          .createOrUpdate(Client('', '19981436342', 'viny@gmail.com', true));
 
       expect(result.fold(id, id), isA<ClientErrors>());
     });
 
     test('should return a ClientError when email is invalid', () async {
-      final result = await useCase.createOrUpdate(Client('nunes', '19981436342', 'viny@', true));
+      final result = await useCase
+          .createOrUpdate(Client('nunes', '19981436342', 'viny@', true));
 
       expect(result.fold(id, id), isA<ClientErrors>());
     });
 
     test('should return a ClientError when phone is invalid', () async {
-      final result =
-          await useCase.createOrUpdate(Client('nunes', '22222222a2', 'viny@gmail.com', true));
+      final result = await useCase.createOrUpdate(
+          Client('nunes', '22222222a2', 'viny@gmail.com', true));
 
       expect(result.fold(id, id), isA<ClientErrors>());
     });
 
     test('should return a Client when is a valid client', () async {
-      final result =
-          await useCase.createOrUpdate(Client('viny', '19981436342', 'viny@gmail.com', true));
+      final result = await useCase.createOrUpdate(
+          Client('viny', '19981436342', 'viny@gmail.com', true));
 
       expect(result.fold(id, id), isA<Client>());
       expect(result.fold((l) => null, (r) => r.name), equals('Nunes'));
     });
   });
 
-  group('tests to delete a client', (){
+  group('tests to delete a client', () {
 
     test('should return true when try to delete a client', () async {
-      final result = await useCase.delete(Client('nunes', '19981436342', 'viny@hotmail.com', true));
+      final result = await useCase
+          .delete(Client('nunes', '19981436342', 'viny@hotmail.com', true));
 
       expect(result.fold(id, id), isA<bool>());
     });
 
-  });
+    test(
+        'should return Client Error when try to delete a client who is already disabled',
+        () async {
+      final result = await useCase
+          .delete(Client('nunes', '19981436342', 'viny@hotmail.com', false));
 
+      expect(result.fold(id, id), isA<ClientErrors>());
+    });
+  });
 }
