75e910bab2e5191e19ec8894d1a464e261b6e55a
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-05-27 17:25:48 -0300

    Tests to the create or update method on in user usecase

---
+++ test/modules/registration/usecases/user_usecase_test.dart
@@ -0,0 +1,68 @@
+import 'package:barbar_manager/modules/registration/domain/entities/user.dart';
+import 'package:barbar_manager/modules/registration/domain/errors/registration_errors.dart';
+import 'package:barbar_manager/modules/registration/domain/repositories/user_repository.dart';
+import 'package:barbar_manager/modules/registration/domain/usecases/impl/user_usercase_impl.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'user_usecase_test.mocks.dart';
+
+@GenerateMocks([UserRepository])
+main() {
+  final repository = MockUserRepository();
+  final usecase = UserUsecaseImpl(repository);
+
+  setUpAll(() {
+    final user = User('id', 'Vinicius Nunes', 'nunes@gmail.com', '123', true);
+    when(repository.createOrUpdate(any))
+        .thenAnswer((realInvocation) async => Right(user));
+
+    when(repository.disable(user))
+        .thenAnswer((realInvocation) async => const Right(true));
+  });
+
+  group('tests to create or update method on usecase', () {
+    test('should return a user when everything is validated on usecase',
+        () async {
+      final result = await usecase.createOrUpdate(
+          User('id', 'ze ramalho', 'ze@gmail.com', '456786', true));
+
+      expect(result.fold(id, id), isA<User>());
+      expect(result.fold((l) => null, (r) => r.fullName),
+          equals('Vinicius Nunes'));
+    });
+
+    test('should return a UserRegistrationError when email is invalid',
+        () async {
+      var result = await usecase
+          .createOrUpdate(User('id', 'ze ramalho', 'ze@gmail', '456786', true));
+
+      expect(result.fold(id, id), isA<UserRegistrationError>());
+
+      result = await usecase.createOrUpdate(
+          User('id', 'ze ramalho', 'zegmail.com', '456786', true));
+
+      expect(result.fold(id, id), isA<UserRegistrationError>());
+    });
+
+    test('should return a UserRegistrationError when password is invalid',
+        () async {
+      var result = await usecase.createOrUpdate(
+          User('id', 'ze ramalho', 'ze@gmail.com', '12345', true));
+
+      expect(result.fold(id, id), isA<UserRegistrationError>());
+
+      result = await usecase
+          .createOrUpdate(User('id', 'ze ramalho', 'ze@gmail.com', 'aa', true));
+
+      expect(result.fold(id, id), isA<UserRegistrationError>());
+
+      result = await usecase
+          .createOrUpdate(User('id', 'ze ramalho', 'ze@gmail.com', '', true));
+
+      expect(result.fold(id, id), isA<UserRegistrationError>());
+    });
+  });
+}
