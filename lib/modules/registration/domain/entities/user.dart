3c6070495405568c35b09188ec70b3590600dc26
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-05-27 17:22:38 -0300

    User to admin the system

---
+++ lib/modules/registration/domain/entities/user.dart
@@ -0,0 +1,10 @@
+class User {
+
+  final String id;
+  final String fullName;
+  final String email;
+  final String password;
+  final bool enabled;
+
+  User(this.id, this.fullName, this.email, this.password, this.enabled);
+}
