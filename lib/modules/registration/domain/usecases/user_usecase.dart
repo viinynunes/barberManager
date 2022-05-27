866ca2b55ea24ed475587ac8a7041f5eefab458e
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-05-27 17:23:06 -0300

    User usecase contract

---
+++ lib/modules/registration/domain/usecases/user_usecase.dart
@@ -0,0 +1,9 @@
+import 'package:barbar_manager/modules/registration/domain/entities/user.dart';
+import 'package:barbar_manager/modules/registration/domain/errors/registration_errors.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UserUsecase {
+  Future<Either<UserRegistrationError, User>> createOrUpdate(User user);
+
+  Future<Either<UserRegistrationError, bool>> disable(User user);
+}
