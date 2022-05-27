06c03f076ef82850dcebcb22bc569af6401318f2
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-05-27 17:59:31 -0300

    Methods for validate each method in usecase

--- lib/modules/registration/domain/utils/validate_user_fields.dart
+++ lib/modules/registration/domain/utils/validate_user_fields.dart
@@ -4,20 +4,32 @@ import 'package:dartz/dartz.dart';
 import 'package:string_validator/string_validator.dart';
 
 class ValidateUserFields {
-
-  static Either<UserRegistrationError, User> validate(User user){
-    if (!isEmail(user.email)){
+  static Either<UserRegistrationError, User> createOrUpdateValidator(
+      User user) {
+    if (!isEmail(user.email)) {
       return Left(UserRegistrationError('Invalid Email'));
     }
 
-    if(user.fullName.isEmpty || user.fullName.length < 2){
+    if (user.fullName.isEmpty || user.fullName.length < 2) {
       return Left(UserRegistrationError('Invalid Full Name'));
     }
 
-    if(user.password.isEmpty || user.password.length < 6){
+    if (user.password.isEmpty || user.password.length < 6) {
       return Left(UserRegistrationError('Invalid Password'));
     }
 
     return Right(user);
+  }
+
+  static Either<UserRegistrationError, bool> disableValidator(User user) {
+    if (user.enabled == false) {
+      return Left(UserRegistrationError('User already disabled'));
+    }
+
+    if (user.id.isEmpty) {
+      return Left(UserRegistrationError('User without id in database'));
+    }
+
+    return const Right(true);
   }
 }
