18d075dcea752f3a2b3190d15c4072d83fe7a762
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-05-31 16:09:03 -0300

    Department usecase contract

---
+++ lib/modules/registration/domain/usecases/department_usecase.dart
@@ -0,0 +1,10 @@
+import 'package:barbar_manager/modules/registration/domain/entities/department.dart';
+import 'package:barbar_manager/modules/registration/domain/errors/registration_errors.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class DepartmentUsecase {
+  Future<Either<RegistrationErrors, Department>> createOrUpdate(
+      Department department);
+
+  Future<Either<RegistrationErrors, bool>> disable(Department department);
+}
