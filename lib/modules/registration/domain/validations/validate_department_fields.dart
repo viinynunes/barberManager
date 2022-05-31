408b178d1a09d977426fdfe2e47553dcdb028088
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-05-31 18:03:39 -0300

    Validate fields in createOrUpdate method on usecase

---
+++ lib/modules/registration/domain/validations/validate_department_fields.dart
@@ -0,0 +1,18 @@
+import 'package:barbar_manager/modules/registration/domain/entities/department.dart';
+import 'package:barbar_manager/modules/registration/domain/errors/registration_errors.dart';
+import 'package:dartz/dartz.dart';
+
+class ValidateDepartmentFields {
+  static Either<RegistrationErrors, Department> createOrUpdateValidation(
+      Department department) {
+    if (department.name.isEmpty || department.name.length < 2) {
+      return Left(DepartmentRegistrationError('Invalid name'));
+    }
+
+    if (department.description.isEmpty) {
+      return Left(DepartmentRegistrationError('Invalid description'));
+    }
+
+    return Right(department);
+  }
+}
