66fc7d13dd56e88203af875cba13a4fd1673b8aa
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-05-31 11:15:47 -0300

    Added email on establishment.dart

--- lib/modules/registration/domain/entities/establishment.dart
+++ lib/modules/registration/domain/entities/establishment.dart
@@ -3,12 +3,13 @@ import 'package:barbar_manager/modules/registration/domain/entities/user.dart';
 class Establishment {
   final String id;
   final String name;
+  final String email;
   final String description;
   final String imgUrl;
 
   final List<User> userList = [];
 
-  Establishment(this.id, this.name, this.description, this.imgUrl);
+  Establishment(this.id, this.name, this.email, this.description, this.imgUrl);
 
   addUser(User user) => userList.add(user);
 
