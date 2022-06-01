51733fbd1e40b60cecdd223f94d64f6e671d25e5
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-06-01 15:48:39 -0300

    Implementation of item usecase

---
+++ lib/modules/registration/domain/usecases/impl/item_usecase_impl.dart
@@ -0,0 +1,34 @@
+import 'package:barbar_manager/modules/registration/domain/entities/item.dart';
+import 'package:barbar_manager/modules/registration/domain/errors/registration_errors.dart';
+import 'package:barbar_manager/modules/registration/domain/repositories/item_repository.dart';
+import 'package:barbar_manager/modules/registration/domain/usecases/item_usecase.dart';
+import 'package:barbar_manager/modules/registration/domain/validations/validate_item_fields.dart';
+import 'package:dartz/dartz.dart';
+
+class ItemUsecaseImpl implements ItemUsecase {
+  final ItemRepository _repository;
+
+  ItemUsecaseImpl(this._repository);
+
+  @override
+  Future<Either<RegistrationErrors, Item>> createOrUpdate(Item item) async {
+    final validator = ValidateItemFields.createOrUpdateValidation(item);
+
+    if (validator.isLeft()) {
+      return validator;
+    }
+
+    return await _repository.createOrUpdate(item);
+  }
+
+  @override
+  Future<Either<RegistrationErrors, bool>> disable(Item item) async {
+    final validator = ValidateItemFields.disableValidation(item);
+
+    if (validator.isLeft()) {
+      return validator;
+    }
+
+    return await _repository.disable(item);
+  }
+}
