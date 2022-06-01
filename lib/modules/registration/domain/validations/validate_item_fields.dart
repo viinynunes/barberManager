eccd4cb1a7097f658971a9f46f8b0d992b9a0649
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-06-01 15:38:48 -0300

    Validation to method create or update of item usecase

---
+++ lib/modules/registration/domain/validations/validate_item_fields.dart
@@ -0,0 +1,42 @@
+import 'package:barbar_manager/modules/registration/domain/entities/item.dart';
+import 'package:barbar_manager/modules/registration/domain/errors/registration_errors.dart';
+import 'package:dartz/dartz.dart';
+
+class ValidateItemFields {
+  static Either<ItemRegistrationError, Item> createOrUpdateValidation(
+      Item item) {
+    if (item.name.isEmpty || item.name.length < 2) {
+      return Left(ItemRegistrationError('Invalid Name'));
+    }
+
+    if (item.description.isEmpty) {
+      return Left(ItemRegistrationError('Invalid Description'));
+    }
+
+    if (item.price.isNegative) {
+      return Left(ItemRegistrationError('Price cannot be negative'));
+    }
+
+    if (item.imgUrl.isEmpty) {
+      return Left(ItemRegistrationError('Invalid image url'));
+    }
+
+    if (item.department.id.isEmpty || !item.department.enabled) {
+      return Left(ItemRegistrationError('Invalid Department'));
+    }
+
+    return Right(item);
+  }
+
+  static Either<ItemRegistrationError, bool> disableValidation(Item item) {
+    if (item.id.isEmpty) {
+      return Left(ItemRegistrationError('Invalid ID'));
+    }
+
+    if (!item.enabled) {
+      return Left(ItemRegistrationError('Item already disabled'));
+    }
+
+    return const Right(true);
+  }
+}
