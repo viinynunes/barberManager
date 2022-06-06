0e8b55ebf592b73584db0cd6cc91067b37f10dd6
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-06-06 14:53:00 -0300

    Validation to reservation exclusive fields

---
+++ lib/modules/registration/domain/validations/validate_reservation_fields.dart
@@ -0,0 +1,37 @@
+import 'package:barbar_manager/modules/registration/domain/entities/item.dart';
+import 'package:barbar_manager/modules/registration/domain/entities/reservation.dart';
+import 'package:dartz/dartz.dart';
+
+import '../errors/registration_errors.dart';
+
+class ValidateReservationFields {
+  static Either<ItemRegistrationError, Item> createOrUpdateValidation(
+      Reservation reservation) {
+    if (reservation.reservationDate.isBefore(DateTime.now())) {
+      return Left(ItemRegistrationError('Date cannot be before now'));
+    }
+
+    if (reservation.reservationDate.day.isNegative ||
+        reservation.reservationDate.month.isNegative ||
+        reservation.reservationDate.year.isNegative) {
+      return Left(ItemRegistrationError('Date cannot be negative'));
+    }
+
+    if (reservation.reservationDate.hour.isNegative ||
+        reservation.reservationDate.minute.isNegative) {
+      return Left(ItemRegistrationError('Hour or minute cannot be negative'));
+    }
+
+    if (reservation.reservationDate.year == 0 ||
+        reservation.reservationDate.month == 0 ||
+        reservation.reservationDate.day == 0) {
+      return Left(ItemRegistrationError('Invalid reservation date'));
+    }
+
+    if (reservation.reservationDate.hour == 0) {
+      return Left(ItemRegistrationError('Hour cannot be empty'));
+    }
+
+    return Right(reservation);
+  }
+}
