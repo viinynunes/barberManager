23c495e2a05319c37a32320a7ab4f24e9aaf2c82
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-06-13 11:00:54 -0300

    Removed removed final from variables

--- lib/modules/registration/domain/entities/item.dart
+++ lib/modules/registration/domain/entities/item.dart
@@ -1,16 +1,18 @@
 import 'package:barbar_manager/modules/registration/domain/entities/department.dart';
+import 'package:barbar_manager/modules/registration/domain/entities/establishment.dart';
 
 class Item {
-  final String id;
-  final String name;
-  final String description;
-  final double price;
-  final String imgUrl;
-  final DateTime registrationDate;
-  final bool enabled;
+  String id;
+  String name;
+  String description;
+  double price;
+  String imgUrl;
+  DateTime registrationDate;
+  bool enabled;
 
-  final Department department;
+  Department department;
+  Establishment establishment;
 
   Item(this.id, this.name, this.description, this.price, this.imgUrl, this.registrationDate,
-      this.enabled, this.department);
+      this.enabled, this.department, this.establishment);
 }
