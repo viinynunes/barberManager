21f7e9587d834e42d97ebe76bada1057118bab85
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-06-17 16:58:12 -0300

    Added var ID

--- lib/modules/registration/domain/entities/client.dart
+++ lib/modules/registration/domain/entities/client.dart
@@ -1,9 +1,10 @@
 class Client {
 
+  String id;
   String name;
   String phoneNumber;
   String email;
   bool enabled;
 
-  Client(this.name, this.phoneNumber, this.email, this.enabled);
+  Client(this.id, this.name, this.phoneNumber, this.email, this.enabled);
 }
