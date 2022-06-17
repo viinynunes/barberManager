b6bbc53592671f525a23b9a55a52a3c63135be3a
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-06-17 11:28:09 -0300

    Removed final from vars

--- lib/modules/registration/domain/entities/user.dart
+++ lib/modules/registration/domain/entities/user.dart
@@ -1,13 +1,13 @@
 import 'package:barbar_manager/modules/registration/domain/entities/establishment.dart';
 
 class User {
-  final String id;
-  final String fullName;
-  final String email;
-  final String password;
-  final bool enabled;
+  String id;
+  String fullName;
+  String email;
+  String password;
+  bool enabled;
 
-  final Establishment establishment;
+  Establishment establishment;
 
   User(this.id, this.fullName, this.email, this.password, this.establishment,
       this.enabled);
